db76854eaff3b7af5d21b1dd10ae3175d057f5c2
guidofioravanti <guidofioravanti@yahoo.it>
2021-01-29 16:13:08 +0100

    Dati no2, nox, pm10, pm25

---
+++ R/no2.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di no2 per 8 stazioni della regione PUGLIA
+#'
+#' @format Un tibble con 8 colonne e 20678 osservazioni
+#'
+#' @usage
+#' no2
+"no2"
---
+++ R/pm25.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di pm25 per 6 stazioni della regione PUGLIA
+#'
+#' @format Un tibble con 8 colonne e 14443 osservazioni
+#'
+#' @usage
+#' pm25
+"pm25"
